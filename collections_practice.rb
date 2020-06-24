8fc6d2aac11014e1df52dd0fd76d072634ac4e33
An Pham <anpham148@gmail.com>
2020-06-24 09:31:07 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,6 +56,11 @@ def sum_array(arr)
 end
 
 def add_s(arr)
-  var1 = arr.delete_at(1)
-  arr.map{|x| x + "s"}.insert(1, var1)
+  arr.each_with_index.collect do |i, e|
+    if i != 2
+      e += "s"
+    end
+  end
+  # var1 = arr.delete_at(1)
+  # arr.map{|x| x + "s"}.insert(1, var1)
 end
