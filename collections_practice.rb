749037348c3e35b422c6fc3018fdf55594eb1c09
An Pham <anpham148@gmail.com>
2020-06-24 09:16:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -56,6 +56,6 @@ def sum_array(arr)
 end
 
 def add_s(arr)
-  var1 = arr.delete_at(1)
-  arr1 = arr.map{|x| x + "s"}.insert(1, var1)
+
+  arr1 = arr.map{|x| x + "s"}.insert(1, arr.delete_at(1))
 end
