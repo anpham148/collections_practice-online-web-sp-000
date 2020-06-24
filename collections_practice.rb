b2b081e1121b2cd9eaec6211ab04f0c020ad4837
An Pham <anpham148@gmail.com>
2020-06-24 08:35:51 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    1
+  elsif a > b
+    -1
+  end
+end
+end
+
+def sort_array_char_count(arr)
+  arr.sort do |a, b|
+  if a.length == b.length
+    0
+  elsif a.length < b.length
+    -1
+  elsif a.length > b.length
+    1
+  end
+end
+end
+
+def swap_elements(arr)
+  arr[1], arr[2] = arr[2], arr[1]
+  arr
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+
+  
+end
